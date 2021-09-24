70aa908516ff489e055cf243f0f6edd83ed2dd02
Tushkov Sergey <rabota054@mail.ru>
2021-09-24 22:03:00 +0500

    v2

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import json
 class VK:
     def __init__(self, token):
         self.token = token
+
     def get_photo(self, quantity_photo=5, album_id='profile'):
         url = 'https://api.vk.com/method/photos.get'
         param = {'extended': 'likes',
@@ -43,11 +44,25 @@ class YaUploader:
     def __init__(self, token: str):
         self.token = token
 
+    def create_folder(self, path):
+        headers = {'Accept': 'application/json',
+                   'Authorization': 'OAuth {}'.format(self.token)}
+        params = {'path': path}
+        url = 'https://cloud-api.yandex.net/put/v1/disk/resources'
+        create_folder = requests.put(url, headers=headers, params=params)
+        if create_folder.status_code == 201:
+            print(f'Директория {path} создана')
+        elif create_folder.status_code == 409:
+            print(f'Директория {path} уже существует')
+        else:
+            print(f'Директория {path} не создана')
+
     def create_json(self, json_data):
         with open("new.json", 'w') as file:
             json.dump(json_data, file, ensure_ascii=False, indent=2)
 
     def upload(self, file_path: str, url_dict):
+        self.create_folder(file_path)
         file_list = []
         json_data = []
         url_number = 0
@@ -62,7 +77,7 @@ class YaUploader:
                 file_list.append(file)
             headers = {'Accept': 'application/json',
                        'Authorization': 'OAuth {}'.format(self.token)}
-            params = {'path': file_path + file,
+            params = {'path': '/' + file_path + '/' + file,
                       'url': file_url}
             url = 'https://cloud-api.yandex.net/post/v1/disk/resources/upload'
             response = requests.post(url, headers=headers, params=params)
@@ -73,16 +88,15 @@ class YaUploader:
                 print(f'Успешно записано {url_number}/{quantity_url}')
             else:
                 print(f'Ошибка {url_number}/{quantity_url}')
-        uploader.create_json(json_data)
+        self.create_json(json_data)
 
-response = VK('')
-url_dict = response.get_photo(quantity_photo=10)
-print(url_dict)
+response = VK('958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008')
+url_dict = response.get_photo(quantity_photo=5)
 
 if __name__ == '__main__':
     token = ''
     uploader = YaUploader(token)
-    result = uploader.upload('/1001/', url_dict)
+    result = uploader.upload('My_folder', url_dict)
 
 
 
